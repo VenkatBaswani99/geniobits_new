5a2172149b916be20350b77dc962fa0ca8ef8b19
VenkatBaswani99 <venkatabaswani99@gmail.com>
2022-09-20 22:07:43 +0530

    Assigment Completed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+
+import GlobalStyles from './components/general/GlobalStyles';
+
+import { ToastContainer } from 'react-toastify';
+
 import App from './App';
+
+import ReactDOM from 'react-dom/client';
+
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(
+
+  document.getElementById('root')
+
+);
+
 root.render(
+
   <React.StrictMode>
+
+    <GlobalStyles />
+
     <App />
+
+    <ToastContainer />
+
   </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
